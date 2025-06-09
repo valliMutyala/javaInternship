53f98e69005258fe32c6780bdea909dd08e97c3d
nagavalliMutyala <mutyalavalli237@gmail.com>
2025-06-09 15:54:33 +0530

    9th june

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Utility {
+    static void greet() {
+        System.out.println("Hello, Valli!");
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        greet();  // Calling without creating an object
+    }
+}
